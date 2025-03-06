e60f8e589d149a9f436a913599eb70784fc33e22
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-03-06 13:21:26 +0100

    EDIT: [Script] Add ServiceLocator logic

--- Runtime/ServiceLocator.cs
+++ Runtime/ServiceLocator.cs
@@ -1,6 +1,57 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ServiceLocator : MonoBehaviour
 {
+    public static ServiceLocator Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.LogWarning("ServiceLocator is not initialized");
+
+            return _instance;
+        }
+    }
+    private static ServiceLocator _instance;
+
+    private Dictionary<Type, object> _services = new Dictionary<Type, object>();
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this;
+    }
     
+    public void Register<T>(T service)
+    {
+        var type = typeof(T);
+        
+        if (_services.ContainsKey(type))
+        {
+            Debug.LogWarning($"Service of type {type} is already registered");
+            return;
+        }
+
+        _services.Add(type, service);
+    }
+    
+    public void Unregister<T>()
+    {
+        var type = typeof(T);
+        
+        if (!_services.ContainsKey(type))
+        {
+            Debug.LogWarning($"Service of type {type} is not registered");
+            return;
+        }
+
+        _services.Remove(type);
+    }
 }
