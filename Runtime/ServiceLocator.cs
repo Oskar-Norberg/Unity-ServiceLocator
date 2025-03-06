7f874f1e6cfd727ac95b0d96b336261e8f527d3b
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-03-06 13:47:55 +0100

    EDIT: [Script] Add GetService function

--- Runtime/ServiceLocator.cs
+++ Runtime/ServiceLocator.cs
@@ -1,57 +1,74 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
-public class ServiceLocator : MonoBehaviour
+namespace ServiceLocator
 {
-    public static ServiceLocator Instance
+    public class ServiceLocator : MonoBehaviour
     {
-        get
+        public static ServiceLocator Instance
         {
-            if (_instance == null)
-                Debug.LogWarning("ServiceLocator is not initialized");
+            get
+            {
+                if (_instance == null)
+                    Debug.LogWarning("ServiceLocator is not initialized");
 
-            return _instance;
+                return _instance;
+            }
         }
-    }
-    private static ServiceLocator _instance;
+        private static ServiceLocator _instance;
 
-    private Dictionary<Type, object> _services = new Dictionary<Type, object>();
+        private Dictionary<Type, object> _services = new Dictionary<Type, object>();
 
-    private void Awake()
-    {
-        if (_instance != null)
+        private void Awake()
         {
-            Destroy(gameObject);
-            return;
-        }
+            if (_instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
 
-        _instance = this;
-    }
+            _instance = this;
+        }
     
-    public void Register<T>(T service)
-    {
-        var type = typeof(T);
-        
-        if (_services.ContainsKey(type))
+        public void Register<T>(T service)
         {
-            Debug.LogWarning($"Service of type {type} is already registered");
-            return;
-        }
+            var type = typeof(T);
+        
+            if (_services.ContainsKey(type))
+            {
+                Debug.LogWarning($"Service of type {type} is already registered");
+                return;
+            }
 
-        _services.Add(type, service);
-    }
+            _services.Add(type, service);
+        }
     
-    public void Unregister<T>()
-    {
-        var type = typeof(T);
-        
-        if (!_services.ContainsKey(type))
+        public void Unregister<T>()
         {
-            Debug.LogWarning($"Service of type {type} is not registered");
-            return;
+            var type = typeof(T);
+        
+            if (!_services.ContainsKey(type))
+            {
+                Debug.LogWarning($"Service of type {type} is not registered");
+                return;
+            }
+
+            _services.Remove(type);
         }
 
-        _services.Remove(type);
+        public T GetService<T>() where T : Object
+        {
+            var service = _services[typeof(T)];
+
+            if (service == null)
+            {
+                Debug.LogWarning($"Service of type {typeof(T)} is not registered");
+                return null;
+            }
+
+            return _services[typeof(T)] as T;
+        }
     }
 }
